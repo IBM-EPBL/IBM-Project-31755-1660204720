2e2de09f2632a8bc07985a8b5eb62bbe99b3abe3
Shruthi <101003553+Shru-th-i@users.noreply.github.com>
2022-11-18 18:52:23 +0530

    Add files via upload

--- Project Development Phase/Sprint4/config.py
+++ Project Development Phase/Sprint4/config.py
@@ -1,1 +1,9 @@
+import os
 
+DEBUG = True
+# TESTING = True
+MAIL_SERVER = 'smtp.googlemail.com'
+MAIL_PORT = 587
+MAIL_USE_TLS = True
+MAIL_USERNAME = os.environ.get('EMAIL_USER')
+MAIL_PASSWORD = os.environ.get('EMAIL_PASS')
